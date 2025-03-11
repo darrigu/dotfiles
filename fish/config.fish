c45ca1056624ac1d959e42da8c0fe5ed248f5444
LeBakaSable <hugogole.ito@gmail.com>
2025-03-11 01:00:05 +0100

    feat: make helix default text editor

--- fish/config.fish
+++ fish/config.fish
@@ -1,6 +1,6 @@
 set fish_greeting
 
-set -gx EDITOR nvim
+set -gx EDITOR hx
 set -gx FZF_DEFAULT_OPTS "--color=bg+:#313244,spinner:#f5e0dc,hl:#f38ba8,fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc,marker:#b4befe,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8,selected-bg:#45475a --multi"
 set -gx OAK_STD_PATH ~/projects/oak/std
 set -gx GOHOME ~/third_party/go
