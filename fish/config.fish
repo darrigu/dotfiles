283f4e49e3d288f59b6880e0df85a09b9a96f51b
darrigu <darrigu.pro@gmail.com>
2026-04-29 19:30:43 +0200

    feat: update

--- fish/config.fish
+++ fish/config.fish
@@ -1,18 +1,7 @@
+set fish_greeting
+
 if status is-interactive
-    set fish_greeting
+    # set -gx LD_LIBRARY_PATH "/home/linuxbrew/.linuxbrew/lib"
 
-    fish_config theme choose "Catppuccin Mocha"
+    bind alt-backspace backward-kill-word
 end
-
-set -gx C_INCLUDE_PATH /usr/lib/erlang/usr/include
-set -gx EDITOR kak
-set -gx CSC_OPTIONS -strict-types
-
-fish_add_path ~/third_party/elixir-ls
-fish_add_path ~/.cache/rebar3/bin
-fish_add_path ~/.local/bin
-fish_add_path ~/.roswell/bin
-fish_add_path ~/.config/v-analyzer/bin
-
-alias nv nvim
-alias k kak
