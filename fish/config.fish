da5ae8350c051a7e686a1e0bcd47740df8e217bb
Hugo Darrieutort-Garcia <h.darrieutortg@gmail.com>
2026-05-17 01:46:46 +0200

    feat: update

--- fish/config.fish
+++ fish/config.fish
@@ -1,8 +1,8 @@
 set fish_greeting
 
-if status is-interactive
-    fish_config theme choose catppuccin-mocha
-    command -q vivid && set -gx LS_COLORS (vivid generate catppuccin-mocha)
+set -gx --prepend PATH "$HOME/.asdf/shims"
 
-    bind alt-backspace backward-kill-word
-end
+fish_config theme choose catppuccin-mocha
+command -q vivid && set -gx LS_COLORS (vivid generate catppuccin-mocha)
+
+bind alt-backspace backward-kill-word
